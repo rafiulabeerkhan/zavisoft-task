0c8a38ed7e8a4ecad522d744e0d4fa8f28ebb5bf
Rafiul Islam Khan <rafiulabeerkhan296@gmail.com>
2026-02-22 11:26:24 +0600

    Spelling fixed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,7 @@
 import Categories from "../components/common/Categories";
 import NewDrops from "../components/common/NewDrops";
 import Reviews from "../components/common/Reviews";
-import doItRight from "../assets/MainContent/doItRight.svg";
+import doItRight from "../assets/MainContent/DoItRight.svg";
 import nikeAirMax from "../assets/MainContent/NikeAirMax.svg";
 
 export default function LandingPage() {
